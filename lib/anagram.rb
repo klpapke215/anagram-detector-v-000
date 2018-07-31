5b4ecdc1f638812e4eec808bab229d30aa79853c
Kristen Papke <klpapke215@gmail.com>
2018-07-31 18:19:58 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize
+    @word = word
+  end
+
+  def match
+    anagrams = []
+    if some_word.split("") == @word
+      anagrams << word
+    else
+      []
+    end
+
+  end 
